7ae0d972c4342bbc4fc13795be0be082352805e3
srijan-keshri <2019mcb1231@iitrpr.ac.in>
2022-02-20 18:38:39 +0530

    some basic changes in app.js file

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
-
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
 const App = () => {
   return (
-    <div>
-      <h1>App</h1>
-    </div>
+    <Container maxWidth="lg">
+      <AppBar />
+    </Container>
   );
 };
 
