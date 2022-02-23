f96a8470abf3156235ea16804bfb5e85ddb57d5d
srijan-keshri <2019mcb1231@iitrpr.ac.in>
2022-02-23 18:16:36 +0530

    added update post functionality

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -3,8 +3,8 @@ import { useState } from "react";
 import { TextField, Button, Typography, Paper } from "@material-ui/core";
 import FileBase from "react-file-base64";
 import { useDispatch } from "react-redux";
-import { createPost } from "../../actions/posts"; 
-const Form = () => {
+import { createPost, updatePost } from "../../actions/posts";
+const Form = ({ setCurrentId, currentId }) => {
   const [postData, setPostData] = useState({
     creator: "",
     title: "",
@@ -14,14 +14,15 @@ const Form = () => {
   });
   const classes = useStyles();
   const dispatch = useDispatch();
-  const clear = () => {
-   
-  };
+  const clear = () => {};
 
-  const handleSubmit = (e) =>
-  {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    dispatch(createPost(postData));
+    if (currentId) {
+      dispatch(updatePost(currentId, postData));
+    } else {
+      dispatch(createPost(postData));
+    }
   };
 
   return (
--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -4,7 +4,7 @@ import { Grid, CircularProgress } from "@material-ui/core";
 import { useSelector } from "react-redux";
 import useStyles from "./Styles.js";
 
-const Posts = () => {
+const Posts = ({ setCurrentId }) => {
   const classes = useStyles();
   const posts = useSelector((state) => state.posts);
   console.log(posts);
@@ -20,7 +20,7 @@ const Posts = () => {
     >
       {posts.map((post) => (
         <Grid key={post._id} item xs={12} sm={6}>
-          <Post post={post} />
+          <Post post={post} setCurrentId={setCurrentId} />
         </Grid>
       ))}
     </Grid>
