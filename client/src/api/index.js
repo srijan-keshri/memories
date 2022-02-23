e58d3c8f51ac2ee5ecfbb2b364c5c7407da71586
srijan-keshri <2019mcb1231@iitrpr.ac.in>
2022-02-23 23:36:36 +0530

    Its final

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const url = "https://memory-social-media.herokuapp.com/posts";
+const url = "http://localhost:5001/posts";
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
